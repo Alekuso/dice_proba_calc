2802749a97c7c2cd29626840ffc23a993feb9d8c
Alex <alexandre.olemans@gmail.com>
2023-01-24 19:09:27 +0100

    Format the code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use rand::prelude::*;
+use rayon::prelude::*;
 use std::io;
 use std::io::Write;
 use std::time::Instant;
-use rand::prelude::*;
-use rayon::prelude::*;
 
 #[macro_use]
 mod scan;
@@ -13,24 +13,31 @@ fn main() {
 
     let dices = scan!(u128);
     println!();
-    
+
     let now = Instant::now();
 
     let cpus = rayon::current_num_threads();
-    let result: usize = (0..cpus).into_par_iter().map(|_| {
-        let mut rng = thread_rng();
-        let mut on_one = 0;
-        for _ in 0..(dices / cpus as u128) {
-            let roll = rng.gen_range(1..=6);
+    let result: usize = (0..cpus)
+        .into_par_iter()
+        .map(|_| {
+            let mut rng = thread_rng();
+            let mut on_one = 0;
+            for _ in 0..(dices / cpus as u128) {
+                let roll = rng.gen_range(1..=6);
 
-            if roll == 1 {
-                on_one += 1;
+                if roll == 1 {
+                    on_one += 1;
+                }
             }
-        }
-        on_one
-
-    }).sum();
+            on_one
+        })
+        .sum();
 
     let time = now.elapsed();
-    println!("P(\"1\")={0}\n\nTime taken: {1:.2}s ({2:.2}ms)", result as f64 / dices as f64, time.as_millis() as f64 / 1000.0, time.as_micros() as f64 / 1000.0);
+    println!(
+        "P(\"1\")={0}\n\nTime taken: {1:.2}s ({2:.2}ms)",
+        result as f64 / dices as f64,
+        time.as_millis() as f64 / 1000.0,
+        time.as_micros() as f64 / 1000.0
+    );
 }
--- src/scan.rs
+++ src/scan.rs
@@ -4,18 +4,15 @@ This macro is kinda like the equivalent of scanf on C.
 USAGE:
 let a = scan!(i32); // scan, expect a number
 let b = scan!(String); // scan, expect a String
-
-Feel free to use
 */
 
 macro_rules! scan {
-  ($x:ty) => {{
-    use std::io;
-      let mut input = String::new();
-      io::stdin().read_line(&mut input).expect("Failed to read line");
-      input
-          .trim()
-          .parse::<$x>()
-          .expect("Invalid input")
-  }};
+    ($x:ty) => {{
+        use std::io;
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        input.trim().parse::<$x>().expect("Invalid input")
+    }};
 }
